66f8c0e63981f4f90f96caa7f7d75b15792e0de8
Nick Lobanov <nk.nicolas142@gmail.com>
2020-04-15 15:52:04 +0300

    hotfix: style

--- script/script.js
+++ script/script.js
@@ -4,11 +4,21 @@ const profileEdit = document.querySelector('.profile__edit');
 const popupCloseIcon = formElement.querySelector('.popup__close-icon');
 const popupSave = formElement.querySelector('.popup__button');
 
+ // Находим поля формы в DOM
+ let formContainer = formElement.querySelector('.popup__container');
+ let nameInput = formContainer.querySelector('.popup__input_type_name');
+ let jobInput = formContainer.querySelector('.popup__input_type_job')
+
+ // Выбираем элементы, куда должны быть вставлены значения полей
+ let profileName = document.querySelector('.profile__name');
+ let profileJob = document.querySelector('.profile__description')
+
+ // функция закрытия popup
 function popupClose() {
     formElement.classList.remove('popup_opened');
 }
 
-let formContainer = formElement.querySelector('.popup__container');
+
 
 //Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
@@ -17,19 +27,10 @@ function formSubmitHandler (evt) {
                                                 // Так мы можем определить свою логику отправки.
                                                 // О том, как это делать, расскажем позже.
 
-    // Находим поля формы в DOM
-    let nameInput = formContainer.querySelector('.popup__input_type_name');
-    let jobInput = formContainer.querySelector('.popup__input_type_job')
-
     // Получите значение полей из свойства value
     nameInput.value
     jobInput.value
     
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-    let profileName = document.querySelector('.profile__name');
-    let profileJob = document.querySelector('.profile__description')
-
     // Вставьте новые значения с помощью textContent
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
@@ -39,10 +40,6 @@ function formSubmitHandler (evt) {
 
 profileEdit.addEventListener('click', function() {
     formElement.classList.add('popup_opened');
-    let nameInput = formContainer.querySelector('.popup__input_type_name');
-    let jobInput = formContainer.querySelector('.popup__input_type_job')
-    let profileName = document.querySelector('.profile__name');
-    let profileJob = document.querySelector('.profile__description')
     nameInput.value = profileName.textContent;
     jobInput.value = profileJob.textContent;
 })
