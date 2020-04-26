ce0596c599eb71db6dd60ebc67a1bd33b4e7daab
Nick Lobanov <nk.nicolas142@gmail.com>
2020-04-26 16:16:25 +0300

    feature: create tamplate

--- script/script.js
+++ script/script.js
@@ -1,3 +1,43 @@
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинск',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+];
+
+const sectionElements = document.querySelector('.elements');
+const templateElement = document.querySelector('#article__template').content;
+
+initialCards.forEach(item => {
+    const newArticle = templateElement.cloneNode(true);
+    newArticle.querySelector('.article__foto').src = item.link;
+    newArticle.querySelector('.article__foto').alt = item.name;
+    newArticle.querySelector('.article__name').textContent = item.name;
+    sectionElements.append(newArticle);
+});
+
+
+
 // Находим форму в DOM
 const formElement = document.querySelector('.popup');
 const profileEdit = document.querySelector('.profile__edit');
@@ -18,14 +58,9 @@ function popupClose() {
     formElement.classList.remove('popup_opened');
 }
 
-
-
-//Обработчик «отправки» формы, хотя пока
-// она никуда отправляться не будет
+//Обработчик «отправки» формы
 function formSubmitHandler (evt) {
     evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
-                                                // Так мы можем определить свою логику отправки.
-                                                // О том, как это делать, расскажем позже.
 
     // Получите значение полей из свойства value
     nameInput.value
